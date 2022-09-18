827020dedb0379837c1d3f616fa7373438dee7d1
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2022-09-18 23:59:10 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,8 +24,8 @@ export default defineConfig({
 	},
   vite: {
     ssr: {
-      external: ["svgo"],
-	  noExternal: ['sharp'],
+      external: ["svgo", "sharp"],
+	  noExternal: ["sharp"],
     },
   },
 });
